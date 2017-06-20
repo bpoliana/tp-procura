287797d23ea7f5bfcb3163d525751df90daf52e3
Barbara Jaber <bpolianajaber@gmail.com>
2017-06-20 17:44:00 -0300

    Novo branch default

--- proc/tables.py
+++ proc/tables.py
@@ -1,11 +1,11 @@
-import django_tables2 as tables
-from .models import Medicine
-from .models import Patient
+#import django_tables2 as tables
+#from .models import Medicine
+#from .models import Patient
 
-class MedicineTable(tables.Table):
-	class Meta:
-		model = Medicine
+#class MedicineTable(tables.Table):
+#	class Meta:
+#		model = Medicine
 
-class PatientTable(tables.Table):
-	class Meta:
-		model = Patient
+#class PatientTable(tables.Table):
+#	class Meta:
+#		model = Patient
